7f9aa3ef3b46f1543c2dc87178d47bed746697b1
maynard.liu <415074476@qq.com>
2025-05-14 13:34:57 +0800

    feat: 改下配置

--- start-production.sh
+++ start-production.sh
@@ -5,9 +5,6 @@ export NODE_ENV=production
 export AUTH_ENABLED=true
 export AUTH_PASSWORD=qq778899
 
-# 创建会话目录并设置权限
-mkdir -p sessions
-chmod 777 sessions
 
 # 启动应用
 node --max-old-space-size=1024 app.js
